8647f8c2d27ccc407eb60cf8a08bc630b102a124
Alexander Shen <shenalexw@gmail.com>
2022-10-26 14:16:42 -0700

    setup mongodb

--- app.js
+++ app.js
@@ -4,38 +4,53 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+require('dotenv').config();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//Mongo Connection credentials
+const mongoDb = require('./mongoDb');
+mongoDb.connectToServer(function (err) {
+  // Set routers
+  var indexRouter = require('./routes/index');
+  var challengeRouter = require('./routes/challenge')
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  // use cors
+  const cors = require("cors");
+  app.use(
+    cors({
+      origin: "*"
+    }))
 
+  // use routes
+  app.use('/', indexRouter);
+  app.use('/challenge', challengeRouter);
+
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+})
 module.exports = app;
---
+++ routes/challenge.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+function createChallenge(name){
+  let challenge = {
+      "name" : name
+  }
+  return challenge
+}
+
+// mongo
+const mongoDb = require('../mongoDb')
+const db = mongoDb.getDb();
+const challenges = 'challenges' //Collection name in MongoDb
+require('dotenv').config()
+
+/* POST Challenge listing. */
+router.post('/', function(req, res) {
+  // Mongo Object ID
+  var name = req.body.name;
+  if (name) {
+      db.collection(challenges).findOne({"name": name}, (err, results) => {
+          if (err) { return console.log(err) }
+          console.log(results);
+          // If a challenge exists then return null
+          if (results !== null) { return res.send(null) }
+
+          // register challenge
+          db.collection(challenges).insertOne(createChallenge(name), (err, results) => {
+              if (err) { return console.log(err) }
+              console.log(results);
+              res.send({ identification: { identification: results["insertedId"] } });
+          });
+      });
+  } else {
+      res.send(null);
+  }
+});
+
+module.exports = router;
