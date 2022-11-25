45411acb75177856f93ed5ea449b8fd1b6d95422
Amele21 <melendra@oregonstate.edu>
2022-11-25 09:37:18 -0800

    Added new routes and controllers for put/delete/get functions

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,10 @@ const {
   loginUser,
   signupUser,
   viewProgress,
+  addProgress,
+  deleteProgress,
+  addWOF,
+  viewWOF,
 } = require("../controllers/userController");
 
 const router = express.Router();
@@ -21,5 +25,18 @@ router.get("/view", requireAuth, viewProgress);
 
 //get the view challenges
 router.get('/view', requireAuth, viewProgress)
+
+//put new progress in user's inProgress property
+router.put('/:id/addProgress/:userid', addProgress)
+
+//delete progress from user's inProgress property
+router.delete('/:id/deleteProgress/:userid', deleteProgress)
+
+//add completed challenge to wall of fame
+router.put('/:id/addWOF/:userid', addWOF)
+
+//get all completed WOF challenges
+router.get('/viewWOF/:userid', viewWOF)
+
 
 module.exports = router;
